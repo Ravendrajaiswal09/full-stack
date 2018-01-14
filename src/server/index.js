944ddfe84b8a0ddc16716a2e4cc8bab35c1f0417
Ravendra <ravendrajaiswal09@gmail.com>
2018-01-15 00:26:07 +0530

    add database

--- src/server/index.js
+++ src/server/index.js
@@ -1,2 +1,2 @@
 require('babel-register');
-require('./webpack.js');
+require('./server.js');
--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,31 @@
 import express from 'express';
 import path from 'path';
 import http from 'http';
+import bodyParser from 'body-parser'
+import route from '../router/index'
+import mongoose from 'mongoose'
 
 const app = express();
 const port = process.env.PORT || 8081;
+
+// connect to mongodb
+mongoose.connect('mongodb://localhost/ninjao')
+mongoose.Promise = global.Promise
+
+
+
+app.use(bodyParser.json())
+app.use('/api', route)
+
+// error Handeling
+
+app.use(function(err, req, res, next){
+  console.log(err)
+  res.send({error: err._message})
+})
+
+
+/*
 
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
@@ -12,7 +34,7 @@ const config = require('../../webpack.config');
 
 const compiler = webpack(config);
 
-app.use(webpackDevMiddleware(compiler, config.devServer));
+app.use(webpackDevMiddleware(compiler));
 app.use(webpackHotMiddleware(compiler));
 
 app.use(express.static(path.join(__dirname, '../..', 'build')));
@@ -20,6 +42,6 @@ app.use(express.static(path.join(__dirname, '../..', 'build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
-
+*/
 console.log(`Server is listening to port: ${port}`);
-app.listen(port);
+app.listen(port); 
