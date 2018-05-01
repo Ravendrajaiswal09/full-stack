d57f0e789419c669822003c9ce2ecb271857e20c
Ravendra <ravendrajaiswal09@gmail.com>
2018-05-01 22:33:10 +0530

    new db

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import express from 'express';
 var router = express.Router();
 var mongojs = require('mongojs');
 var ObjectId = require('mongodb').ObjectID;
-var db = mongojs('mongodb://ravendra:1234@ds111059.mlab.com:11059/my_task')
+var db = ''
 
 router.get('/task', function(req, res, next){
     db.collection('task').find().toArray(function(err, data){
